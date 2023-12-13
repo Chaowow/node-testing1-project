562f6e0ba50a3549a9e67511e71241ea379a008b
Frankie <frankiejosefej@gmail.com>
2023-12-13 15:37:21 -0500

    met mvp

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ class Seasons {
    * [Exercise 5A] Seasons creates a seasons object
    */
   constructor() {
-    // ✨ initialize whatever properties are needed
+    this.seasons = ['summer', 'fall', 'winter', 'spring'];
+    this.currentSeason = 0;
   }
 
   /**
@@ -90,7 +91,13 @@ class Seasons {
    * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    const result = this.seasons[this.currentSeason];
+    if (this.currentSeason === 3) {
+      this.currentSeason = 0
+    } else {
+      this.currentSeason++;
+    }
+    return result;
   }
 }
 
@@ -102,9 +109,10 @@ class Car {
    * @param {number} mpg - miles the car can drive per gallon of gas
    */
   constructor(name, tankSize, mpg) {
-    this.odometer = 0 // car initilizes with zero miles
-    this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.odometer = 0; // car initilizes with zero miles
+    this.tank = tankSize; // car initiazes full of gas
+    this.tankSize = tankSize;
+    this.mpg = mpg;
   }
 
   /**
@@ -121,7 +129,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const milesAbleToDrive = this.tank * this.mpg;
+    if (distance <= milesAbleToDrive) {
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance / this.mpg);
+    } else {
+      this.tank = 0;
+      this.odometer = this.odometer + milesAbleToDrive;
+    }
+    return this.odometer;
   }
 
   /**
@@ -136,7 +152,13 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank;
+    if (gallons <= gallonsThatFit) {
+      this.tank = this.tank + gallons;
+    } else {
+      this.tank = this.tankSize;
+    }
+    return this.tank * this.mpg;
   }
 }
 
@@ -154,7 +176,10 @@ class Car {
  * })
  */
 function isEvenNumberAsync(number) {
-  // ✨ implement
+  if (number % 2 === 0) {
+    return Promise.resolve(true);
+  }
+  return Promise.resolve(false);
 }
 
 module.exports = {
